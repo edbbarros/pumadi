3fb5d52f4429baf4eb8f3bd7636dc29ce1fca1fc
Eduardo Barros <39862684+edbbarros@users.noreply.github.com>
2023-03-02 15:51:45 -0300

    Create file of images imports

---
+++ src/Index.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.svg'
+import branding from './assets/branding.png';
+import estrategia from './assets/ESTRATEGIA.png'
+import execucao from './assets/EXECUCAO.png'
+import midiaOnline from './assets/MIDIAONLINE.png'
+import propaganda from './assets/PROPAGANDA.png'
+import design from './assets/DESIGN.png'
+import webLogo from './assets/DESENVOLVIMENTOWEB.png'
+import Marcas from './assets/MARCASDOSCLIENTES.png'
+import Background2 from './assets/BG2.png'
+
+
+export { logo, branding, estrategia, execucao, midiaOnline, propaganda, design, webLogo, Marcas, Background2 }
