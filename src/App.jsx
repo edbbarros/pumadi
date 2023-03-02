68158ef94df49bb30aa2934471b777f05bd34f46
Eduardo Barros <39862684+edbbarros@users.noreply.github.com>
2023-03-02 15:52:27 -0300

    Introduce new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Global from "./styles/Global";
-import { HomeText, Navbar, NavItem, NavList, Section } from "./styles/Styles";
-import logo from './assets/logo.svg'
+import { About, AboutMkt, AboutMkt1, Card, Card1, Card2, Card3, Card4, HomeCard, HomeText, Navbar, NavItem, NavList, Section, Sectionfooter, SectionForm } from "./styles/Styles";
 import { PrimaryFontText } from "./styles/Fonts";
+import { logo, branding, estrategia, execucao, midiaOnline, propaganda, design, webLogo, Marcas } from './Index.jsx';
 
 const App = () => {
   return (
@@ -11,7 +11,6 @@ const App = () => {
       <Section>
         <Navbar>
           <img src={logo} alt="Logo" />
-
           <NavList>
             <NavItem>Inicio</NavItem>
             <NavItem>O que fazemos</NavItem>
@@ -21,18 +20,225 @@ const App = () => {
           </NavList>
         </Navbar>
 
-        <HomeText>
+        <HomeText >
           <h1>SOLUÇÕES <strong>CRIATIVAS</strong> E <strong>FUNCIONAIS</strong> PARA O SEU NEGÓCIO
           </h1>
           <p>Somos uma Agência de Marketing especializada em criação e desenvolvimento denaming,
             branding e tudo mais que uma empresa precisa para se relacionar da melhor forma com o
             mercado</p>
           {/* <button>saiba mais</button> */}
+
         </HomeText>
-
-
       </Section>
+
+      <HomeCard>
+        <h1>NA PUMADI UNIMOS ESSES <strong>3 PILARES</strong></h1>
+        <h3>
+          Para gerar resultados reais para nossos clientes
+        </h3>
+      </HomeCard>
+
+      <Card>
+        <div className="containerArea">
+          <div className="container">
+            <div className="circle">
+              <img src={branding} alt="Image branding" />
+            </div>
+            <div className="container2">
+              <h1>BRANDING</h1>
+              <h3>reproduzimos através de recursos visuais as
+                ideias e comportamento da sua empresa que
+                muitas vezes não são possíveis transmitir com
+                palavras.</h3>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="circle">
+              <img src={estrategia} alt="Image branding" />
+            </div>
+            <div className="container2">
+              <h1>ESTRATÉGIA</h1>
+              <h3>A estratégia surge para gerenciar as ações e
+                objetivos definidos para seu negócio,
+                considerando os ambientes que envolvem a
+                organização,mantendo o trabalho alinhado
+                aos objetivos.</h3>
+            </div>
+          </div>
+          <div className="container">
+            <div className="circle">
+              <img src={execucao} alt="Image branding" />
+            </div>
+            <div className="container2">
+              <h1>EXECUÇÃO</h1>
+              <h3>Execução é fundamental para a estratégia, e
+                deve moldá-la. Nenhuma estratégia que valha
+                a pena pode ser planejada sem levarem
+                conta a habilidade da organização em
+                executá-la.</h3>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <About>
+        <h1>O QUE <strong>FAZEMOS?</strong></h1>
+      </About>
+      <Card1>
+        <div className="containerArea1">
+
+          <div className="container1">
+            <img src={midiaOnline} alt="Image branding" />
+            <h1>MÍDIA ONLINE</h1>
+            <h3>E-mail marketing, Banners,
+              Estratégia de Marketing Online,
+              Google AdGestão de Mídias Sociais:
+              Facebook, Instagram, Linkedin, Blogs.</h3>
+          </div>
+
+          <div className="container1">
+            <img src={propaganda} alt="Image branding" />
+            <h1>PROPAGANDA</h1>
+            <h3>Planejamento de Comunicação, Planejamento de Mídia. </h3>
+          </div>
+
+          <div className="container1">
+            <img src={design} alt="Image branding" />
+            <h1>DESIGN</h1>
+            <h3>Identidade visual, key visual, lançamentos,  Social media, cria</h3>
+          </div>
+
+          <div className="container1">
+            <img src={webLogo} alt="Image branding" />
+            <h1>DESENVOLVIMENTO WEB</h1>
+            <h3>Sites, Landing Pages E-commerce, Automações</h3>
+          </div>
+        </div>
+      </Card1>
+
+      <Card2>
+        <div className="containerCardLarge">
+          <div className="cardLarge border">
+            <h1>EQUIPE <strong>PREPARADA</strong> E <strong>
+              PRONTA</strong> PARA FAZER SEU NEGÓCIO DECOLAR!</h1>
+            <button>Quero conhecer</button>
+          </div>
+        </div>
+      </Card2>
+
+      <AboutMkt>
+        <h1> CLIENTES QUE JÁ PASSARAM PELA PUMADI </h1>
+        <h3> Nossos cases de sucesso </h3>
+      </AboutMkt>
+
+      <Card3>
+        <div className="containerCardLarge1">
+          <div className="cardLarge1 border1">
+            <img src={Marcas} alt="Marcas dos clientes" />
+          </div>
+        </div>
+      </Card3>
+
+      <AboutMkt1>
+        <h1>NOSSOS PLANOS</h1>
+      </AboutMkt1>
+
+      <Card4>
+        <div className="ContainerArea1">
+          <div className="card1">
+            <div className="rectangle">
+              <h1>BÁSICO</h1>
+            </div>
+            <div className="card2">
+              <h1><strong>R$299,</strong>00<sub>/MÊS</sub></h1>
+            </div>
+          </div>
+          <div className="card1">
+            <div className="rectangle">
+              <h1>INTERMEDIÁRIO</h1>
+            </div>
+            <div className="card2">
+              <h1><strong>R$799,</strong>00<sub>/MÊS</sub></h1>
+            </div>
+          </div>
+          <div className="card1">
+            <div className="rectangle">
+              <h1>COMPLETO</h1>
+            </div>
+            <div className="card2">
+              <h1><strong>R$1249,</strong>00<sub>/MÊS</sub></h1>
+            </div>
+          </div>
+        </div>
+
+      </Card4>
+
+      {/* <CatButton>
+        <button>QUERO CONHECER</button>
+      </CatButton> */}
+
+      <SectionForm>
+        <h1>FAÇA O SEU NEGÓCIO DECOLAR <strong>
+          AGORA!</strong></h1>
+        <div className="containerForm border">
+          <label htmlFor="">
+            Primeiro nome
+          </label>
+          <input type="text" placeholder="Digite seu nome" />
+
+          <label htmlFor="">
+            E-mail
+          </label>
+          <input type="text" placeholder="Digite seu e-mail" />
+
+          <label htmlFor="">
+            DDD + Whatsapp
+          </label>
+          <input type="text" placeholder="Digite seu número" />
+
+          <label htmlFor="">
+            Cidade
+          </label>
+          <input type="text" placeholder="Digite sua cidade" />
+
+          <label htmlFor="">
+            Plano
+          </label>
+          <select name="" id="">
+            <option>Selecione o seu plano</option>
+            <option value=""> Básico</option>
+            <option value=""> Intermediário</option>
+            <option value=""> Completo</option>
+          </select>
+
+          <button>
+            Solicitar Agora!
+          </button>
+
+        </div>
+      </SectionForm>
+
+      <Sectionfooter id="Inicio">
+        <div className="containerFooter">
+          <ul>
+            <li><strong>Contato</strong></li>
+            <li>Pina | Recife - PE</li>
+            <li>(81) 4042-9575 | (81) 98220-7312</li>
+            <li>mkt.pumadi@gmail.com</li>
+            <li>CNPJ: 49.102.568/0001-02</li>
+          </ul>
+
+          <img src={logo} alt="Logo" />
+        </div>
+        <hr />
+        <div className="Copyright">
+          <p> &copy; 2023 Desenvolvido por: Pumadi</p>
+        </div>
+      </Sectionfooter>
+
     </>
+
+
   )
 }
 
--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 import Background1 from '../assets/BG1.png';
+import { Background2 } from '../Index.jsx';
+
 
 export const Section = styled.section`
 background-image: url(${Background1}); 
+background-repeat: no-repeat;
+background-size: 100% 100%;
 height: 100vh;
 `
 
@@ -19,11 +23,12 @@ export const NavList = styled.ul`
   list-style: none;
   background-color: #8710c3;
   border-radius: 5rem;
-  gap: 3rem;
-  padding: 1rem;
+  gap: 1rem;
+
 `;
 
 export const NavItem = styled.li`
+  padding: 1rem;
   font-size: 1.2rem;
   font-weight: 300;
   color: #ffffff;
@@ -32,6 +37,8 @@ export const NavItem = styled.li`
   &:hover {
   background-color: #ffffff;
   color: #8710c3;
+  border-radius: 5rem;
+  font-weight: bold;
   }
 `;
 
@@ -39,15 +46,434 @@ export const HomeText = styled.header`
 padding: 4rem;
 color: #ffffff;
 
-h1{
-  font-size: 2.0rem;
-  font-weight:300;
-  width: 35rem;
+  h1{
+    font-size: 2rem;
+    font-weight:300;
+    width: 35rem;
+  }
+  p{
+    margin-top: 2rem;
+    font-size: 1rem;
+    font-weight:300;
+    width: 25rem;
+  }
+`
+
+export const HomeCard = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #ffffff;
+
+  h1{
+    font-size: 2.5rem;
+    font-weight:300;
+
 }
-p{
-  margin-top: 2rem;
-  font-size: 1rem;
-  font-weight:300;
+  h3{
+    margin-top: 1rem;
+    font-size: 1rem;
+    font-weight:300;
+    margin-bottom: 5rem;
+  }
+`
+
+export const Card = styled.section`
+.containerArea{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 3rem;
+  color: #8710c3;
+  }
+.container{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  height: 30rem;
   width: 25rem;
+  background-color: #8710c3;
+  border-radius: 2rem;
+  margin-bottom: 10rem;
+  position: relative;
+    .circle{
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        width: 13rem;
+        height: 13rem;
+        border-radius: 50%;
+        background-color: #660099;
+        position: absolute;
+        top: 25px;
+          img {
+            height: 8rem;
+            width: 8rem;
+          }
+    }
+  }
+
+.container2{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 25rem;
+  width: 25rem;
+  background-color: #ffffff;
+  border-radius: 2rem;
+  }
+   h1{
+    margin-top:5rem;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+   h3{    
+    margin-top: 1rem;
+    font-size: 1rem;
+    font-weight:300;
+    width: 20rem;
+  }
+`
+export const About = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #ffffff;
+   h1{
+    /* margin-top:5rem; */
+    font-size: 2rem;
+    font-weight: 700;
+  }
+`
+
+export const Card1 = styled.section`
+.containerArea1{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  color: #ffffff;
+
 }
+
+.container1{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 25rem;
+  width: 20rem;
+  /* margin-bottom: 2rem; */
+
+    img {
+      height: 6rem;
+      width: 6rem;
+    }
+    h1 {
+      margin-top: 1rem;
+      font-size: 1.5rem;
+      font-weight: 700;
+  }
+    h3 {    
+      margin-top: 0.5rem;
+      font-size: 0.8rem;
+      font-weight: 300;
+      width: 13rem;
+      /* margin-bottom: 1rem; */
+
+  }
+}
+`
+
+export const Card2 = styled.section`
+  .containerCardLarge{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    margin-bottom: 5rem;
+  }
+
+  .cardLarge{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    width: 70rem;
+    height: 25rem;
+    background-color: #8710c3;
+  }
+
+  .border{
+    box-shadow: 0.5rem 0.5rem 0px 1px #fff;
+    border-radius: 2rem;
+  }
+
+  h1{ 
+    font-size: 2rem;
+    width:40rem;
+    margin-bottom: 2rem;
+    font-weight: 300;
+  }
+  button{
+    color: #fff;
+    width: 15rem;
+    background-color: #660099;
+    border-style: none;
+    border-radius: 4rem;
+    height: 2rem;
+    font-size: 1rem;
+    font-weight: 700;
+  }
+`
+
+export const AboutMkt = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+
+    h1{ 
+      font-size: 2rem;
+      font-weight: 700;
+      margin-bottom: 1rem;
+    }
+
+    h3{ 
+      font-size: 1rem;
+      font-weight: 300;
+      margin-bottom: 4rem;
+    }
+`
+
+export const Card3 = styled.section`
+   .containerCardLarge1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #000000;
+    margin-bottom: 5rem;
+  }
+
+  .cardLarge1{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    width: 70rem;
+    height: 25rem;
+    background-color: #fff;
+  }
+
+  .border1{
+    box-shadow: 0.5rem 0.5rem 0px 1px #8710c3;
+    border-radius: 2rem;
+  }
+
+  img {
+    width: 60rem;
+  }
+`
+
+export const AboutMkt1 = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  margin-bottom: 5rem;
+    h1{
+      font-size: 2rem;
+      font-weight: 700;
+    }
+
+`
+
+export const Card4 = styled.section`
+  .ContainerArea1{
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 10rem;
+      gap: 3rem;
+      }
+
+  .card1{
+    position: relative;    
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    height: 15rem;
+    width: 20rem;
+    background-color:#8710c3 ;
+    border-radius: 3rem;
+
+      .rectangle{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        background-color:#660099;
+        width: 17rem;
+        height:4rem;
+        border-radius: 3rem;
+        position: absolute;
+        top: 35px;
+      }
+  }
+  .card2 {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 11.5rem;
+    width: 20rem;
+    background-color: #ffffff;
+    border-radius: 3rem;
+      h1{
+      color: #660099;
+      font-size: 1.8rem;
+      font-weight: 700;
+    }
+  }
+  `
+// export const CatButton = styled.button`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     margin-bottom: 10rem;
+
+//   button {
+//     background-color: #8710c3;
+//     color: #fff;
+//     border-style: none;
+//     width: 10rem;
+//     height: 5rem;
+
+//    }
+// `
+
+export const SectionForm = styled.section`
+background-image: url(${Background2}); 
+background-repeat: no-repeat;
+background-size:  100% 100%;
+height: 100vh;
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+
+  h1 {
+    align-items: center;
+    font-size: 4rem;
+    font-weight: 300;
+    color: #fff;
+    width: 40rem;
+  }
+  .containerForm {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    height: 42rem;
+    width: 33rem;
+    background-color: #8710c3;
+    border-radius: 3rem;
+    margin-bottom: 5rem;
+
+      label {
+      align-self:flex-start;
+      margin-left: 4rem; 
+      font-size: 1.5rem;
+      color: #fff;
+      font-weight: 700;
+    }
+
+    input {
+      height: 3rem;
+      width: 28rem;
+      padding: 1rem;
+      font-size:1rem;
+      border-radius: 0.5rem;
+      border: none;
+      color: #fff;
+      background-color: #660099;
+     
+    }
+
+    select { 
+      height: 3rem;
+      width: 28rem;
+      padding: 1rem;
+      font-size:1rem;
+      border-radius: 0.5rem;
+      border: none;
+      color: #fff;
+      background-color: #660099;
+      font-size: 1rem;
+      font-weight: 300;
+    }
+    option {
+      font-size: 1rem;
+      font-weight: 700;
+    }
+
+    button {
+      margin-top: 1rem;
+    color: #fff;
+    width: 15rem;
+    background-color: #660099;
+    border-style: none;
+    border-radius: 4rem;
+    height: 2rem;
+    font-size: 1rem;
+    font-weight: 700;
+    box-shadow: 0.2rem 0.2rem 0px 0px #fff;
+    }
+
+  }
+  .border{
+    box-shadow: 0.5rem 0.5rem 0px 0px #fff;
+    border-radius: 3rem;
+  }
+`
+
+export const Sectionfooter = styled.footer`
+  .containerFooter {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    margin-bottom: 5rem;
+    color: #fff;
+
+      ul {
+        list-style: none;
+      }
+
+      li {
+        padding: 0.5rem;
+      }
+
+      img {
+        height:15rem
+      }
+  }
+      hr { 
+        margin-bottom: 2rem;
+      }
+
+      .Copyright {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        font-weight: 300;
+        margin-bottom: 2rem;
+      }
+
 `
