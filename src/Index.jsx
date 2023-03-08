dd7331ad88098bb7d1866ae295d3d1fb41d2ad7d
Eduardo Barros <39862684+edbbarros@users.noreply.github.com>
2023-03-08 16:41:29 -0300

    Correction Build

--- src/Index.jsx
+++ src/Index.jsx
@@ -11,9 +11,8 @@ import Background2 from './assets/BG2.png'
 import InstagramLogo from './assets/instagram.png'
 import LinkedinLogo from './assets/linkedin.png'
 import BehanceLogo from './assets/behance.png'
-import favIcon from './assets/favicon.png'
 import Basico from './assets/basico.png'
 import Intermediario from './assets/intermediario.png'
 import Completo from './assets/completo.png'
 
-export { logo, branding, estrategia, execucao, midiaOnline, propaganda, design, webLogo, Marcas, Background2, InstagramLogo, LinkedinLogo, BehanceLogo, favIcon, Basico, Intermediario, Completo }
+export { logo, branding, estrategia, execucao, midiaOnline, propaganda, design, webLogo, Marcas, Background2, InstagramLogo, LinkedinLogo, BehanceLogo, Basico, Intermediario, Completo }
